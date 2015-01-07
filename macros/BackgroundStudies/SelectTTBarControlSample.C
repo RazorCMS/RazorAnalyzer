40351941ba988e7c60b7c6a19a3dc3f9bea620cd
SiXie <Si.Xie@cern.ch>
2015-01-08 00:32:10 +0100

    add additional control samples for cross checks

--- macros/BackgroundStudies/SelectTTBarControlSample.C
+++ macros/BackgroundStudies/SelectTTBarControlSample.C
@@ -32,7 +32,7 @@ int color[10] = {kRed, kGreen+2, kBlue, kMagenta, kBlack, kBlack, kBlack, kBlack
 //=== MAIN MACRO ================================================================================================= 
 
 
-void RunSelectTTBarControlSample( vector<string> inputfiles, vector<string> processLabels, int option = -1, string label = "") {
+void RunSelectDileptonTTBarControlSample( vector<string> inputfiles, vector<string> processLabels, int option = -1, string label = "") {
   
   string Label = "";
   if (label != "") Label = "_" + label;
@@ -143,6 +143,8 @@ void RunSelectTTBarControlSample( vector<string> inputfiles, vector<string> proc
       histMRVsRsq[i]->Fill(events->MR,events->Rsq, events->weight*lumi);
     }
   }
+
+  cout << "here1\n";
 
   //--------------------------------------------------------------------------------------------------------------
   // Compute Expected Statistical Uncertainty
@@ -303,15 +305,310 @@ void RunSelectTTBarControlSample( vector<string> inputfiles, vector<string> proc
 }
 
 
+
+void RunSelectSingleLeptonTTBarControlSample( vector<string> inputfiles, vector<string> processLabels, int option = -1, string label = "") {
+  
+  string Label = "";
+  if (label != "") Label = "_" + label;
+
+
+  //--------------------------------------------------------------------------------------------------------------
+  // Settings 
+  //============================================================================================================== 
+  bool printdebug = false;
+  double lumi = 5000;
+
+  //*****************************************************************************************
+  //Make some histograms
+  //*****************************************************************************************
+
+  double MRBins[8] = {300, 400, 500, 750, 1000, 1500, 2000, 4000};
+  double RsqBins[10] = {0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.75,1.0,1.5};
+
+  vector<TH1F*> histMR;
+  vector<TH1F*> histRsq;
+  vector<TH1F*> histMT;
+  vector<TH2F*> histMRVsRsq;
+
+  assert (inputfiles.size() == processLabels.size());
+  for (uint i=0; i < inputfiles.size(); ++i) {
+    histMR.push_back(new TH1F(Form("histMR_%s",processLabels[i].c_str()), "; M_{R} [GeV/c^{2}]; Number of Events", 100, 0, 3000));
+    histRsq.push_back(new TH1F(Form("histRsq_%s",processLabels[i].c_str()), "; R^{2} ; Number of Events", 100, 0, 2.0));
+    histMT.push_back(new TH1F(Form("histMT_%s",processLabels[i].c_str()), "; M_{ll} [GeV/c^{2}]; Number of Events", 100, 0, 1000));
+    histMRVsRsq.push_back(new TH2F(Form("histMRVsRsq_%s",processLabels[i].c_str()), "; M_{R} [GeV/c^{2}]; R^{2}; Number of Events", 7, MRBins, 9, RsqBins));
+  }
+ 
+  //*******************************************************************************************
+  //Read file
+  //*******************************************************************************************                
+  for (uint i=0; i < inputfiles.size(); ++i) {
+    ControlSampleEvents *events = new ControlSampleEvents;
+    events->LoadTree(inputfiles[i].c_str());
+
+    cout << "process: " << processLabels[i] << " | Total Entries: " << events->tree_->GetEntries() << "\n";
+    for(UInt_t ientry=0; ientry < events->tree_->GetEntries(); ientry++) {       	
+      events->tree_->GetEntry(ientry);
+      
+      if (ientry % 100000 == 0) cout << "Event " << ientry << endl;      
+      //if (ientry > 1000000) break;
+
+      //******************************
+      //Selection Cuts 
+      //******************************
+      if (!( (abs(events->lep1Type) == 11 || abs(events->lep1Type) == 13))	     
+	  ) continue;
+
+      //lepton selection
+      if (! (events->lep1.Pt() > 30 && events->lep1PassTight)
+	  ) continue;
+
+      //2nd lepton veto
+      if (! (events->lep2.Pt() <= 0 
+	     ||	   
+	     ( (events->lep1+events->lep2).M() < 66 || (events->lep1+events->lep2).M() > 116) )
+	  )
+	continue;
+
+      //MT cuts
+      if (!(events->lep1MT > 30 && events->lep1MT < 100)) continue;
+
+      //Razor signal region cuts
+      if (!(events->MR > 300 && events->Rsq > 0.1)) continue;
+    
+      //******************************
+      //Options
+      //******************************
+      //1 or more CSV Medium tag
+      if (option == 0 &&
+	  !(events->NBJetsMedium >= 1)
+	  ) continue;
+      
+      //2 or more CSV Loose tag
+      if (option == 1 &&
+	  !(events->NBJetsLoose >= 2)
+	  ) continue;
+
+      //******************************
+      //Fill histograms
+      //******************************
+      histMR[i]->Fill(events->MR,events->weight*lumi);
+      histRsq[i]->Fill(events->Rsq,events->weight*lumi);
+      histMT[i]->Fill(events->lep1MT,events->weight*lumi);      
+      histMRVsRsq[i]->Fill(events->MR,events->Rsq, events->weight*lumi);
+    }
+  }
+
+  cout << "here1\n";
+  //--------------------------------------------------------------------------------------------------------------
+  // Compute Expected Statistical Uncertainty
+  //==============================================================================================================
+  TH2F *statUnc_MRVsRsq = (TH2F*)(histMRVsRsq[0]->Clone("statUnc_MRVsRsq"));
+  for (int i=0; i<statUnc_MRVsRsq->GetXaxis()->GetNbins()+1;i++) {
+    for (int j=0; j<statUnc_MRVsRsq->GetYaxis()->GetNbins()+1;j++) {
+      if (statUnc_MRVsRsq->GetBinContent(i,j) > 1) {
+  	statUnc_MRVsRsq->SetBinContent(i,j,1.0 / sqrt(statUnc_MRVsRsq->GetBinContent(i,j)));  	
+      } else {
+  	statUnc_MRVsRsq->SetBinContent(i,j,0);
+      }
+    }
+  }
+
+ 
+  //--------------------------------------------------------------------------------------------------------------
+  // Make Plots
+  //==============================================================================================================
+
+
+  //--------------------------------------------------------------------------------------------------------------
+  // Draw
+  //==============================================================================================================
+  TCanvas *cv =0;
+  TLegend *legend = 0;
+
+  cout << "here2\n";
+  //*******************************************************************************************
+  //MR
+  //*******************************************************************************************
+  cv = new TCanvas("cv","cv", 800,600);
+  legend = new TLegend(0.60,0.54,0.90,0.84);
+  legend->SetTextSize(0.03);
+  legend->SetBorderSize(0);
+  legend->SetFillStyle(0);
+
+  THStack *stackMR = new THStack();
+  for (int i = histMR.size()-1; i >= 0; --i) {
+   histMR[i]->SetFillColor(color[i]);
+    histMR[i]->SetFillStyle(1001);
+
+    if ( histMR[i]->Integral() > 0) {
+      stackMR->Add(histMR[i]);
+    }
+  }
+  for (uint i = 0 ; i < histMR.size(); ++i) {
+    legend->AddEntry(histMR[i],(processLabels[i]).c_str(), "F");
+  }
+
+  if (stackMR->GetHists()->GetEntries() > 0) {
+    stackMR->Draw();
+    stackMR->GetHistogram()->GetXaxis()->SetTitle(((TH1F*)(stackMR->GetHists()->At(0)))->GetXaxis()->GetTitle());
+    stackMR->GetHistogram()->GetYaxis()->SetTitle(((TH1F*)(stackMR->GetHists()->At(0)))->GetYaxis()->GetTitle());
+    legend->Draw();
+    cv->SaveAs(Form("Razor_TTBarCrossCheckRegion_MR%s.gif",Label.c_str()));
+  }
+
+  cout << "here3\n";
+  //*******************************************************************************************
+  //Rsq
+  //*******************************************************************************************
+  cv = new TCanvas("cv","cv", 800,600);
+  legend = new TLegend(0.60,0.54,0.90,0.84);
+  legend->SetTextSize(0.03);
+  legend->SetBorderSize(0);
+  legend->SetFillStyle(0);
+
+  THStack *stackRsq = new THStack();
+  for (int i = histRsq.size()-1; i >= 0; --i) {
+    histRsq[i]->SetFillColor(color[i]);
+    histRsq[i]->SetFillStyle(1001);
+
+    if ( histRsq[i]->Integral() > 0) {
+      stackRsq->Add(histRsq[i]);
+    }
+  }
+  for (uint i = 0 ; i < histRsq.size(); ++i) {
+    legend->AddEntry(histRsq[i],(processLabels[i]).c_str(), "F");
+  }
+
+  if (stackRsq->GetHists()->GetEntries() > 0) {
+    stackRsq->Draw();
+    stackRsq->GetHistogram()->GetXaxis()->SetTitle(((TH1F*)(stackRsq->GetHists()->At(0)))->GetXaxis()->GetTitle());
+    stackRsq->GetHistogram()->GetYaxis()->SetTitle(((TH1F*)(stackRsq->GetHists()->At(0)))->GetYaxis()->GetTitle());
+    legend->Draw();
+    cv->SaveAs(Form("Razor_TTBarCrossCheckRegion_Rsq%s.gif",Label.c_str()));
+  }
+
+  cout << "here4\n";
+  //*******************************************************************************************
+  //MT
+  //*******************************************************************************************
+  cv = new TCanvas("cv","cv", 800,600);
+  legend = new TLegend(0.60,0.54,0.90,0.84);
+  legend->SetTextSize(0.03);
+  legend->SetBorderSize(0);
+  legend->SetFillStyle(0);
+
+  THStack *stackMT = new THStack();
+  for (int i = histMT.size()-1; i >= 0; --i) {
+    histMT[i]->SetFillColor(color[i]);
+    histMT[i]->SetFillStyle(1001);
+
+    if ( histMT[i]->Integral() > 0) {
+      stackMT->Add(histMT[i]);
+    }
+  }
+  for (uint i = 0 ; i < histMT.size(); ++i) {
+    legend->AddEntry(histMT[i],(processLabels[i]).c_str(), "F");
+  }
+
+  if (stackMT->GetHists()->GetEntries() > 0) {
+    stackMT->Draw();
+    stackMT->GetHistogram()->GetXaxis()->SetTitle(((TH1F*)(stackMT->GetHists()->At(0)))->GetXaxis()->GetTitle());
+    stackMT->GetHistogram()->GetYaxis()->SetTitle(((TH1F*)(stackMT->GetHists()->At(0)))->GetYaxis()->GetTitle());
+    legend->Draw();
+    cv->SaveAs(Form("Razor_TTBarCrossCheckRegion_MT%s.gif",Label.c_str()));
+  }
+
+
+  cout << "here6\n";
+  //*******************************************************************************************
+  //MR Vs Rsq
+  //*******************************************************************************************
+  cv = new TCanvas("cv","cv", 800,600);
+  histMRVsRsq[0]->SetStats(false);
+  histMRVsRsq[0]->Draw("colz");
+  cv->SaveAs(Form("Razor_TTBarCrossCheckRegion_EventCountsTTBar_MRVsRsq%s.gif",Label.c_str()));
+
+  cv = new TCanvas("cv","cv", 800,600);
+  statUnc_MRVsRsq->SetStats(false);
+  statUnc_MRVsRsq->Draw("colz");
+  cv->SaveAs(Form("Razor_TTBarCrossCheckRegion_StatUncTTBar_MRVsRsq%s.gif",Label.c_str()));
+
+  //--------------------------------------------------------------------------------------------------------------
+  // Tables
+  //==============================================================================================================
+  cout << "For Luminosity = " << lumi << " pb^-1\n";
+  cout << "Yields : MR > 300 && Rsq > 0.1\n";
+
+
+   cout << "here6\n";
+ //--------------------------------------------------------------------------------------------------------------
+  // Output
+  //==============================================================================================================
+  TFile *file = TFile::Open(("TTBarCrossCheckRegionPlots"+Label+".root").c_str(), "UPDATE");
+  file->cd();
+
+  for(int i=0; i<int(inputfiles.size()); i++) {
+    file->WriteTObject(histMR[i], Form("histMR_%s",processLabels[i].c_str()), "WriteDelete");
+    file->WriteTObject(histRsq[i], Form("histRsq_%s",processLabels[i].c_str()), "WriteDelete");
+    file->WriteTObject(histMT[i], Form("histMT_%s",processLabels[i].c_str()), "WriteDelete");
+  }
+  file->WriteTObject(stackMR, "stackMR", "WriteDelete");
+  file->WriteTObject(stackRsq, "stackRsq", "WriteDelete");
+  file->WriteTObject(stackMT, "stackMT", "WriteDelete");
+
+  for(int i=0; i<int(histMRVsRsq.size()); i++) {
+    file->WriteTObject(histMRVsRsq[i], Form("histMRVsRsq_%s",processLabels[i].c_str()), "WriteDelete");
+  }
+  file->WriteTObject(statUnc_MRVsRsq,"statUnc_MRVsRsq_TTBar","WriteDelete");
+  cout << "here7\n";
+
+  file->Close();
+  delete file;       
+
+}
+
+
+
+
+
 void SelectTTBarControlSample( int option = -1, string label = "") {
 
   vector<string> inputfiles;
   vector<string> processLabels;
 
-  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_DileptonSkim_TTJets_25ns_weighted.root");  
-  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_DileptonSkim_DYJetsToLL_HT100ToInf_25ns_weighted.root");
-  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_DileptonSkim_WJetsToLNu_HT100ToInf_25ns_weighted.root");
-  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_DileptonSkim_SingleTop_25ns_weighted.root"); 
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/skim/RazorControlRegions_DileptonSkim_TTJets_25ns_weighted.root");  
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/skim/RazorControlRegions_DileptonSkim_DYJetsToLL_HT100ToInf_25ns_weighted.root");
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/skim/RazorControlRegions_DileptonSkim_WJetsToLNu_HT100ToInf_25ns_weighted.root");
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/skim/RazorControlRegions_DileptonSkim_SingleTop_25ns_weighted.root"); 
+  // inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_Multiboson_25ns_weighted.root");
+
+  processLabels.push_back("TTJets");
+  processLabels.push_back("DYJetsToLL");
+  processLabels.push_back("WJetsToLNu");
+  processLabels.push_back("SingleTop");
+  // processLabels.push_back("Multiboson");
+
+  //*********************************************************************
+  //Dilepton Control Region
+  //*********************************************************************
+  //RunSelectDileptonTTBarControlSample(inputfiles,processLabels,-1,"all");
+  //RunSelectDileptonTTBarControlSample(inputfiles,processLabels,0,"emu");
+  //RunSelectDileptonTTBarControlSample(inputfiles,processLabels,1,"eemumu");
+  //RunSelectDileptonTTBarControlSample(inputfiles,processLabels,2,"ee");
+  //RunSelectDileptonTTBarControlSample(inputfiles,processLabels,3,"mumu");
+  
+
+
+  //*********************************************************************
+  //Single Lepton Control Region
+  //*********************************************************************
+  inputfiles.clear();
+  processLabels.clear();
+
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_LeptonPlusMTCutSkim_TTJets_25ns_weighted.root");  
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_LeptonPlusMTCutSkim_DYJetsToLL_HT100ToInf_25ns_weighted.root");
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_LeptonPlusMTCutSkim_WJetsToLNu_HT100ToInf_25ns_weighted.root");
+  inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_LeptonPlusMTCutSkim_SingleTop_25ns_weighted.root"); 
   inputfiles.push_back("/afs/cern.ch/work/s/sixie/public/Run2SUSY/RazorControlRegions/RazorControlRegions_Multiboson_25ns_weighted.root");
 
   processLabels.push_back("TTJets");
@@ -320,10 +617,8 @@ void SelectTTBarControlSample( int option = -1, string label = "") {
   processLabels.push_back("SingleTop");
   processLabels.push_back("Multiboson");
 
-  RunSelectTTBarControlSample(inputfiles,processLabels,-1,"all");
-  // RunSelectTTBarControlSample(inputfiles,processLabels,0,"emu");
-  // RunSelectTTBarControlSample(inputfiles,processLabels,1,"eemumu");
-  //RunSelectTTBarControlSample(inputfiles,processLabels,2,"ee");
-  //RunSelectTTBarControlSample(inputfiles,processLabels,3,"mumu");
+
+  RunSelectSingleLeptonTTBarControlSample(inputfiles,processLabels,0,"1CSVMTag");
+  RunSelectSingleLeptonTTBarControlSample(inputfiles,processLabels,1,"2CSVLTag");
 
 }
