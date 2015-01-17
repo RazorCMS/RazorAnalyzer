a7683bbabcb9125ee734083207b204cf681d82d1
SiXie <Si.Xie@cern.ch>
2015-01-17 16:46:32 +0100

    add ID and Iso only selectors

--- src/RazorAuxElectron.cc
+++ src/RazorAuxElectron.cc
@@ -130,3 +130,142 @@ bool RazorAnalyzer::isMVANonTrigVetoElectron(int i){
   return pass;
 
 }
+
+
+
+
+bool RazorAnalyzer::passLooseElectronID(int i){
+  bool pass = false;
+  if(fabs(eleEta_SC[i]) < 1.479) {
+    if ( fabs(ele_dEta[i]) < 0.0181
+	 && fabs(ele_dPhi[i]) < 0.0936
+	 && eleFull5x5SigmaIetaIeta[i] < 0.0123
+	 && ele_HoverE[i] < 0.141
+	 && fabs(ele_d0[i]) < 0.0166
+	 && fabs(ele_dZ[i]) < 0.54342
+	 && ele_OneOverEminusOneOverP[i] < 0.1353
+	 && ele_PassConvVeto[i]
+	 && ele_MissHits[i] < 1
+	) {
+      pass = true;
+    }
+  } else {
+    if (fabs(ele_dEta[i]) < 0.0124
+	 && fabs(ele_dPhi[i]) < 0.0642
+	 && eleFull5x5SigmaIetaIeta[i] < 0.035
+	 && ele_HoverE[i] < 0.1115
+	 && fabs(ele_d0[i]) < 0.098
+	 && fabs(ele_dZ[i]) < 0.9187
+	 && ele_OneOverEminusOneOverP[i] < 0.1443
+	&& ele_PassConvVeto[i]
+	 && ele_MissHits[i] < 1
+	) {
+      pass = true;
+    }
+  } 
+  return pass;
+}
+
+bool RazorAnalyzer::passTightElectronID(int i){
+  bool pass = false;
+  if(fabs(eleEta_SC[i]) < 1.479) {
+    if ( fabs(ele_dEta[i]) < 0.0091
+	 && fabs(ele_dPhi[i]) < 0.031
+	 && eleFull5x5SigmaIetaIeta[i] < 0.0106
+	 && ele_HoverE[i] < 0.0532
+	 && fabs(ele_d0[i]) < 0.0126
+	 && fabs(ele_dZ[i]) < 0.0116
+	 && ele_OneOverEminusOneOverP[i] < 0.0609
+	 && ele_PassConvVeto[i]
+	 && ele_MissHits[i] < 1
+	) {
+      pass = true;
+    }
+  } else {
+    if (fabs(ele_dEta[i]) < 0.0106
+	 && fabs(ele_dPhi[i]) < 0.0359
+	 && eleFull5x5SigmaIetaIeta[i] < 0.0305
+	 && ele_HoverE[i] < 0.0835
+	 && fabs(ele_d0[i]) < 0.0163
+	 && fabs(ele_dZ[i]) < 0.5999
+	 && ele_OneOverEminusOneOverP[i] < 0.1126
+	&& ele_PassConvVeto[i]
+	 && ele_MissHits[i] < 1
+	) {
+      pass = true;
+    }
+  } 
+  return pass;
+}
+
+bool RazorAnalyzer::passMVANonTrigVetoElectronID(int i){
+
+  Int_t subdet = 0;
+  if (fabs(eleEta_SC[i]) < 1.479) subdet = 0;
+  else subdet = 1;
+  Int_t ptBin = 0;
+  if (elePt[i] > 10.0) ptBin = 1;
+
+  Double_t MVACut = -999;
+  if (subdet == 0 && ptBin == 0) MVACut = 0.0;
+  if (subdet == 1 && ptBin == 0) MVACut = 0.6;
+  if (subdet == 0 && ptBin == 1) MVACut = -0.3;
+  if (subdet == 1 && ptBin == 1) MVACut = 0.5;
+
+  bool pass = false;
+  if (ele_IDMVANonTrig[i] > MVACut) {
+    pass = true;
+  }
+
+
+  return pass;
+
+}
+
+
+bool RazorAnalyzer::passLooseElectronIso(int i){
+  bool pass = false;
+  if(fabs(eleEta_SC[i]) < 1.479) {
+    if ( ele_relIsoDBetaCorr[i] < 0.24	
+	) {
+      pass = true;
+    }
+  } else {
+    if ( ele_relIsoDBetaCorr[i] < 0.3529
+	) {
+      pass = true;
+    }
+  } 
+  return pass;
+}
+
+bool RazorAnalyzer::passTightElectronIso(int i){
+  bool pass = false;
+  if(fabs(eleEta_SC[i]) < 1.479) {
+    if ( ele_relIsoDBetaCorr[i] < 0.1649
+	) {
+      pass = true;
+    }
+  } else {
+    if ( ele_relIsoDBetaCorr[i] < 0.2075
+	) {
+      pass = true;
+    }
+  } 
+  return pass;
+}
+
+bool RazorAnalyzer::passMVANonTrigVetoElectronIso(int i){
+ 
+  bool pass = false;
+  if (  ( (elePt[i] > 20 && ele_relIsoDBetaCorr[i] < 0.3)
+	  ||
+	  (elePt[i] <= 20 && ele_relIsoDBetaCorr[i]*elePt[i] < 5)
+	  )
+	) {
+    pass = true;
+  }
+
+  return pass;
+
+}
--- src/RazorAuxMuon.cc
+++ src/RazorAuxMuon.cc
@@ -39,3 +39,67 @@ bool RazorAnalyzer::isTightMuon(int i){
   }
   return pass;
 }   
+
+
+
+bool RazorAnalyzer::passVetoMuonID(int i){
+  bool pass = false;
+  if (muonIsLoose[i] 
+      && fabs(muon_ip3dSignificance[i]) < 4
+      ) {
+    pass = true;
+  }
+  return pass;
+}
+
+bool RazorAnalyzer::passLooseMuonID(int i){
+  bool pass = false;
+  if (muonIsLoose[i] 
+      && fabs(muon_ip3dSignificance[i]) < 4
+      ) {
+    pass = true;
+  }
+  return pass;
+}
+
+bool RazorAnalyzer::passTightMuonID(int i){
+  bool pass = false;
+  if (
+      muonIsTight[i] 
+      && fabs(muon_ip3dSignificance[i]) < 4
+      && abs(muon_d0[i]) < 0.2
+      ) {
+    pass = true;
+  }
+  return pass;
+}   
+
+bool RazorAnalyzer::passVetoMuonIso(int i){
+  bool pass = false;
+  if ( ( ( muonPt[i] > 20 && muon_relIso04DBetaCorr[i] < 0.4 )
+	 ||
+	 ( muonPt[i] <= 20 && muon_relIso04DBetaCorr[i]*muonPt[i] < 10 ) 
+	 )
+       ) {
+    pass = true;
+  }
+  return pass;
+}
+
+bool RazorAnalyzer::passLooseMuonIso(int i){
+  bool pass = false;
+  if ( muon_relIso04DBetaCorr[i] < 0.2
+      ) {
+    pass = true;
+  }
+  return pass;
+}
+
+bool RazorAnalyzer::passTightMuonIso(int i){
+  bool pass = false;
+  if ( muon_relIso04DBetaCorr[i] < 0.12     
+      ) {
+    pass = true;
+  }
+  return pass;
+}   
