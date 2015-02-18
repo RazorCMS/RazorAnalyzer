f76e2b0add35b6246dda832840d67d55566fbd80
Javier <jduarte@caltech.edu>
2015-02-18 06:11:16 +0100

    plot asimov dataset

--- python/PlotFit.py
+++ python/PlotFit.py
@@ -28,6 +28,9 @@ if __name__ == '__main__':
                   help="input file with fit")
     parser.add_option('-d','--dir',dest="outDir",default="./",type="string",
                   help="Output directory to store plots")
+    parser.add_option('--asimov',dest="asimov",default=False,action='store_true',
+                  help="use asimov dataset instead")
+
 
 
     (options,args) = parser.parse_args()
@@ -59,6 +62,8 @@ if __name__ == '__main__':
     
                                      
     th1x = w.var('th1x')
+    CMS_channel = w.cat('CMS_channel')
+    w.cat('CMS_channel').setLabel(box)
     fit_b.Print('v')
     fit_s.Print('v')
     
@@ -72,6 +77,15 @@ if __name__ == '__main__':
         w.var(p.GetName()).setError(p.getError())
         print "INITIALIZE PARAMETER %s = %f +- %f"%(p.GetName(),p.getVal(),p.getError())
     w.var('r').setVal(0.)
+    
+    asimov = w.pdf('_model_bonly_').generateBinned(rt.RooArgSet(CMS_channel,th1x),rt.RooFit.Asimov(),rt.RooFit.Name('Asimov'))
+
+    if options.asimov:
+        w.var('r').setConstant(True)
+        fit_b = w.pdf('_model_bonly_').fitTo(asimov,rt.RooFit.Save(),rt.RooFit.SumW2Error(True))
+        w.var('r').setConstant(False)
+        fit_s = w.pdf('model_s').fitTo(asimov,rt.RooFit.Save(),rt.RooFit.SumW2Error(True))
+    
 
     norm1b = w.var('shapeBkg_%s_TTj1b_%s__norm'%(box,box)).getVal()
     norm2b = w.var('shapeBkg_%s_TTj2b_%s__norm'%(box,box)).getVal()
@@ -79,19 +93,21 @@ if __name__ == '__main__':
     
     w.var('shapeBkg_%s_TTj2b_%s__norm'%(box,box)).setVal(0.)
     w.var('shapeBkg_%s_TTj3b_%s__norm'%(box,box)).setVal(0.)
-    asimov1b = w.pdf('pdf_bin%s_nuis'%box).generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('Asimov1b'))
+    asimov1b = w.pdf('_model_bonly_').generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('Asimov1b'))
     w.var('shapeBkg_%s_TTj1b_%s__norm'%(box,box)).setVal(0.)
     w.var('shapeBkg_%s_TTj2b_%s__norm'%(box,box)).setVal(norm2b)
-    asimov2b = w.pdf('pdf_bin%s_nuis'%box).generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('Asimov2b'))
+    asimov2b = w.pdf('_model_bonly_').generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('Asimov2b'))
     w.var('shapeBkg_%s_TTj2b_%s__norm'%(box,box)).setVal(0.)
     w.var('shapeBkg_%s_TTj3b_%s__norm'%(box,box)).setVal(norm3b)
-    asimov3b = w.pdf('pdf_bin%s_nuis'%box).generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('Asimov3b'))
+    asimov3b = w.pdf('_model_bonly_').generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('Asimov3b'))
 
     hist1b = asimov1b.createHistogram("hist1b",th1x)
     hist2b = asimov2b.createHistogram("hist2b",th1x)
     hist3b = asimov3b.createHistogram("hist3b",th1x)
     
-    stack = rt.THStack("hs","hist stacked")
+    hist1b.GetXaxis().SetTitle("th1x")
+    hist1b.GetYaxis().SetTitle("Events")
+    stack = rt.THStack("hs","best-fit b-only")
     hist1b.SetFillColor(rt.kBlue)
     stack.Add(hist1b)
     hist2b.SetFillColor(rt.kViolet)
@@ -101,18 +117,31 @@ if __name__ == '__main__':
     stack.SetMinimum(0.01)
     stack.SetMaximum(2.*w.data('data_obs').sumEntries())
 
-    w.data('data_obs').plotOn(th1xFrame,rt.RooFit.Invisible())
+    if options.asimov:
+        asimov.plotOn(th1xFrame,rt.RooFit.Invisible())
+    else:
+        w.data('data_obs').plotOn(th1xFrame,rt.RooFit.Invisible())
     th1xFrame.addObject(stack,'hist')
-    #th1xFrame.addTH1(hist2b,'hist')
-    #th1xFrame.addTH1(hist3b,'hist')
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrame,rt.RooFit.LineColor(rt.kBlack))
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrame,rt.RooFit.Components('shapeBkg_%s_TTj1b_%s'%(box,box)),rt.RooFit.LineColor(rt.kBlue))
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrame,rt.RooFit.Components('shapeBkg_%s_TTj2b_%s'%(box,box)),rt.RooFit.LineColor(rt.kViolet))
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrame,rt.RooFit.Components('shapeBkg_%s_TTj3b_%s'%(box,box)),rt.RooFit.LineColor(rt.kGreen))
-    w.data('data_obs').plotOn(th1xFrame)
+    if options.asimov:
+        asimov.plotOn(th1xFrame)
+    else:
+        w.data('data_obs').plotOn(th1xFrame)
+        
     th1xFrame.SetMinimum(0.01)
     th1xFrame.SetMaximum(2.*w.data('data_obs').sumEntries())
+    th1xFrame.GetXaxis().SetTitle('th1x')
+    th1xFrame.GetYaxis().SetTitle('Events')
     th1xFrame.Draw()
+
+    
+    leg = rt.TLegend(0.56,0.74,0.89,0.89)
+    leg.SetTextFont(42)
+    leg.SetFillColor(rt.kWhite)
+    leg.SetLineColor(rt.kWhite)
+    leg.AddEntry(hist1b,"1 b-tag","f")
+    leg.AddEntry(hist2b,"2 b-tag","f")
+    leg.AddEntry(hist3b,"#geq3 b-tag","f")
+    leg.Draw()
     
     c.Print('th1xFrame_b.pdf')
 
@@ -131,23 +160,23 @@ if __name__ == '__main__':
     w.var('r').setVal(0.)
     w.var('shapeBkg_%s_TTj2b_%s__norm'%(box,box)).setVal(0.)
     w.var('shapeBkg_%s_TTj3b_%s__norm'%(box,box)).setVal(0.)
-    asimov1b = w.pdf('pdf_bin%s_nuis'%box).generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovSB1b'))
+    asimov1b = w.pdf('model_s').generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovSB1b'))
     w.var('shapeBkg_%s_TTj1b_%s__norm'%(box,box)).setVal(0.)
     w.var('shapeBkg_%s_TTj2b_%s__norm'%(box,box)).setVal(norm2b)
-    asimov2b = w.pdf('pdf_bin%s_nuis'%box).generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovSB2b'))
+    asimov2b = w.pdf('model_s').generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovSB2b'))
     w.var('shapeBkg_%s_TTj2b_%s__norm'%(box,box)).setVal(0.)
     w.var('shapeBkg_%s_TTj3b_%s__norm'%(box,box)).setVal(norm3b)
-    asimov3b = w.pdf('pdf_bin%s_nuis'%box).generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovSB3b'))
+    asimov3b = w.pdf('model_s').generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovSB3b'))
     w.var('shapeBkg_%s_TTj3b_%s__norm'%(box,box)).setVal(0.)
     w.var('r').setVal(rVal)
-    asimovs = w.pdf('pdf_bin%s_nuis'%box).generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovS'))
+    asimovs = w.pdf('model_s').generateBinned(rt.RooArgSet(th1x),rt.RooFit.Asimov(),rt.RooFit.Name('AsimovS'))
 
     hist1b = asimov1b.createHistogram("hist1bsig",th1x)
     hist2b = asimov2b.createHistogram("hist2bsig",th1x)
     hist3b = asimov3b.createHistogram("hist3bsig",th1x)
     histsig = asimovs.createHistogram("histsig",th1x)
 
-    stack = rt.THStack("hsig","hist stacked")
+    stack = rt.THStack("hsig","best-fit s+b")
     hist1b.SetFillColor(rt.kBlue)
     stack.Add(hist1b)
     hist2b.SetFillColor(rt.kViolet)
@@ -160,18 +189,26 @@ if __name__ == '__main__':
     stack.SetMinimum(0.01)
     stack.SetMaximum(2.*w.data('data_obs').sumEntries())
         
-    w.data('data_obs').plotOn(th1xFrameClone,rt.RooFit.Invisible())
+    if options.asimov:
+        asimov.plotOn(th1xFrameClone,rt.RooFit.Invisible())
+    else:
+        w.data('data_obs').plotOn(th1xFrameClone,rt.RooFit.Invisible())
     th1xFrameClone.addObject(stack,'hist')
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrameClone,rt.RooFit.LineColor(rt.kBlack))
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrameClone,rt.RooFit.Components('shapeBkg_%s_TTj1b_%s'%(box,box)),rt.RooFit.LineColor(rt.kBlue))
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrameClone,rt.RooFit.Components('shapeBkg_%s_TTj2b_%s'%(box,box)),rt.RooFit.LineColor(rt.kViolet))
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrameClone,rt.RooFit.Components('shapeBkg_%s_TTj3b_%s'%(box,box)),rt.RooFit.LineColor(rt.kGreen))
-    #w.pdf('pdf_bin%s_nuis'%box).plotOn(th1xFrameClone,rt.RooFit.Components('shapeSig_%s_%s_%sPdf'%(box,model,box)),rt.RooFit.LineColor(rt.kRed))
-    w.data('data_obs').plotOn(th1xFrameClone)
-    th1xFrameClone.SetMinimum(0.01)
-    th1xFrameClone.SetMaximum(2.*w.data('data_obs').sumEntries())
+    if options.asimov:
+        asimov.plotOn(th1xFrameClone)
+    else:
+        w.data('data_obs').plotOn(th1xFrameClone)
     th1xFrameClone.Draw()
-    
+    leg = rt.TLegend(0.56,0.69,0.89,0.89)
+    leg.SetTextFont(42)
+    leg.SetFillColor(rt.kWhite)
+    leg.SetLineColor(rt.kWhite)
+    leg.AddEntry(hist1b,"1 b-tag","f")
+    leg.AddEntry(hist2b,"2 b-tag","f")
+    leg.AddEntry(hist3b,"#geq3 b-tag","f")
+    leg.AddEntry(histsig,"r = %.3f #pm %.3f"%(w.var('r').getVal(),w.var('r').getError()),"f")
+    leg.Draw()
+
     c.Print('th1xFrame_s.pdf')
     
     
