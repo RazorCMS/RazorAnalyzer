130d1c63ac32bf51211c56182320cd9c5d71f5d2
Cristian <cristian.morgoth@gmail.com>
2015-05-05 01:46:09 +0200

    final changes to HggRazor

--- src/RazorAuxPhoton.cc
+++ src/RazorAuxPhoton.cc
@@ -116,102 +116,122 @@ void RazorAnalyzer::getPhotonEffAreaRun1( float eta, double& effAreaChHad, doubl
     }
 };
 
-bool RazorAnalyzer::photonPassIsoRun1( int i )
+bool RazorAnalyzer::photonPassIsoRun1( int i , bool _debug )
 {
   //Define variables
   float eta = pho_superClusterEta[i];
-  //float pt  = phoPt[i]*phoE[i]/pho_RegressionE[i]; 
-  float pt  = phoPt[i];
+  //float pt  = phoE[i]/cosh(phoEta[i]); 
+  float pt  = phoPt[i];//default pt
   bool _isEB = false;
-  if ( fabs( eta ) < 1.44 ) _isEB = true;
+  if ( fabs( eta ) < 1.48 ) _isEB = true;
   //get effective area for isolation calculations                                                
   double effAreaChargedHadrons = 0.0;
   double effAreaNeutralHadrons = 0.0;
   double effAreaPhotons = 0.0;
   getPhotonEffAreaRun1( eta, effAreaChargedHadrons, effAreaNeutralHadrons, effAreaPhotons );
-  /*
-  std::cout << "chA:" << effAreaChargedHadrons << " nA: " << effAreaNeutralHadrons << " phoA: " << effAreaPhotons << std::endl;
-  std::cout << "chHad: " << pho_sumChargedHadronPt[i] << std::endl;
-  std::cout << "nHad: " << pho_sumNeutralHadronEt[i] << std::endl;
-  std::cout << "pho: " << pho_sumPhotonEt[i] << std::endl;
-  std::cout << "pu: " << fixedGridRhoAll << std::endl;
-  */
+  if ( _debug )
+    {
+      std::cout << "chA:" << effAreaChargedHadrons << " nA: " << effAreaNeutralHadrons << " phoA: " << effAreaPhotons << std::endl;
+      std::cout << "chHad: " << pho_sumChargedHadronPt[i] << std::endl;
+      std::cout << "nHad: " << pho_sumNeutralHadronEt[i] << std::endl;
+      std::cout << "pho: " << pho_sumPhotonEt[i] << std::endl;
+      std::cout << "pu: " << fixedGridRhoAll << std::endl;
+    }
   //Compute Photon Isolation
   //Rho corrected PF charged hadron isolation
   double PFIsoCorrected_chHad = max(pho_sumChargedHadronPt[i] - fixedGridRhoAll*effAreaChargedHadrons, 0.);
-  //std::cout << "chHad Iso: " << PFIsoCorrected_chHad << std::endl;
   //Rho corrected PF neutral hadron isolation                                                                              
   double PFIsoCorrected_nHad = max(pho_sumNeutralHadronEt[i] - fixedGridRhoAll*effAreaNeutralHadrons, 0.);
-  //std::cout << "nHad Iso: " <<PFIsoCorrected_nHad <<std::endl;
   //Rho corrected PF photon isolation                                                              
   double PFIsoCorrected_pho = max(pho_sumPhotonEt[i] - fixedGridRhoAll*effAreaPhotons, 0.);
-  //std::cout << "pho Iso: " << PFIsoCorrected_pho <<std::endl;
+  if ( _debug )
+    {
+      std::cout << "chHad Iso: " << PFIsoCorrected_chHad << std::endl;
+      std::cout << "nHad Iso: " <<PFIsoCorrected_nHad <<std::endl;
+      std::cout << "pho Iso: " << PFIsoCorrected_pho <<std::endl; 
+    }
   
   //Apply Isolation
   if ( _isEB )
     {
-      //std::cout << "==EB==" << std::endl;
-      //std::cout << "pass chHad iso?" << std::endl;
-      //std::cout << "chHad Cut: " << EB_GetPFchHadIsoCut() << std::endl;
-      if ( PFIsoCorrected_chHad > EB_GetPFchHadIsoCut() ) return false;
-      //std::cout<< "pass nHad iso?" << std::endl;
-      //std::cout << "nHad Cut: " << EB_GetPFnHadIsoCut( pt ) << std::endl;
-      if ( PFIsoCorrected_nHad > EB_GetPFnHadIsoCut( pt ) ) return false;
-      //std::cout << "Pho Cut: " << EB_GetPFphoIsoCut( pt ) << std::endl;
-      //std::cout<< "pass pho iso?" <<std::endl;
-      if ( PFIsoCorrected_pho > EB_GetPFphoIsoCut( pt ) ) return false;
-      //std::cout<< "EB Yes" <<std::endl;
+      if ( PFIsoCorrected_chHad > EB_GetPFchHadIsoCut() )
+	{
+	  std::cout << "EB, Iso: failed chHadIso, cut @ " << EB_GetPFchHadIsoCut() << std::endl;
+	  return false;
+	}
+      if ( PFIsoCorrected_nHad > EB_GetPFnHadIsoCut( pt ) )
+	{
+	  std::cout << "EB, Iso: failed nHadIso, cut @ " << EB_GetPFnHadIsoCut( pt ) << std::endl;
+	  return false;
+	}
+      if ( PFIsoCorrected_pho > EB_GetPFphoIsoCut( pt ) )
+	{
+	  std::cout << "EB, Iso: failed phoIso, cut @ " << EB_GetPFphoIsoCut( pt ) << std::endl;
+	  return false;
+	}
     }
   else
     {
-      //std::cout << "==EE==" << std::endl;
-      //std::cout << "pass chHad iso?" << std::endl;
-      //std::cout << "chHad Cut: " << EE_GetPFchHadIsoCut() << std::endl;
-      if ( PFIsoCorrected_chHad > EE_GetPFchHadIsoCut() ) return false;
-      //std::cout << "nHad Cut: " << EE_GetPFnHadIsoCut( pt ) << std::endl;
-      //std::cout<< "pass nHad iso?" <<std::endl;
-      if ( PFIsoCorrected_nHad > EE_GetPFnHadIsoCut( pt ) ) return false;
-      //std::cout<< "EE Yes" <<std::endl;
-      //No Photn Isolation requirement;
+      if ( PFIsoCorrected_chHad > EE_GetPFchHadIsoCut() )
+	{
+	  std::cout << "EE, Iso: failed chHadIso, cut @ " << EE_GetPFchHadIsoCut() << std::endl;
+	  return false;
+	}
+      if ( PFIsoCorrected_nHad > EE_GetPFnHadIsoCut( pt ) )
+	{
+	  std::cout << "EE, Iso: failed nHadIso, cut @ " << EE_GetPFnHadIsoCut( pt ) << std::endl;
+	  return false;
+	}
     }
   
   return true;
 };
 
 //Cut Based Photon ID WP 90/85 (EB/EE) recommendation from EGamma: https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedPhotonID2012
-bool RazorAnalyzer::isGoodPhotonRun1( int i, bool _iso = false)
+bool RazorAnalyzer::isGoodPhotonRun1( int i, bool _iso = false, bool _debug = false)
 {
   bool _isEB = false;
-  if ( fabs( pho_superClusterEta[i] ) < 1.44 ) _isEB = true;
+  if ( fabs( pho_superClusterEta[i] ) < 1.48 ) _isEB = true;
   if ( _isEB )
     {
       //EB
-      //std::cout<< "=====EB====" << std::endl;
-      //std::cout << "pass Ele Veto?" << std::endl;
-      if ( EB_EleVeto && !passEleVetoRun1( i ) ) return false;
-      //std::cout << "yes!, pass HoverE?" << std::endl;
-      if ( pho_HoverE[i] > EB_HoverECut ) return false;// HoverE Cut
-      //std::cout << "yes!, pass ietaieta?" << std::endl;
-      if ( phoSigmaIetaIeta[i] > EB_SigmaIetaIetaCut ) return false;// SigmaIetaIeta Cut
-      //if ( phoFull5x5SigmaIetaIeta[i] > EB_SigmaIetaIetaCut ) return false;
-      //std::cout << "yes!" << std::endl;
-      if ( _iso && !photonPassIsoRun1( i ) ) return false;//Apply Isolation if flag (_iso) is true
+      if ( EB_EleVeto && !passEleVetoRun1( i ) ) 
+	{
+	  if ( _debug ) std::cout << "EB, ID: failed CSEV" << std::endl;
+	  return false;
+	}
+      if ( pho_HoverE[i] > EB_HoverECut )
+	{
+	  if ( _debug ) std::cout << "EB, ID: failed HoveE: " << pho_HoverE[i] << std::endl;
+	  return false;// HoverE Cut
+	}
+      if ( phoSigmaIetaIeta[i] > EB_SigmaIetaIetaCut )
+	{
+	  if ( _debug ) std::cout << "EB, ID: failed SigmaIetaIeta: " << phoSigmaIetaIeta[i] <<  std::endl;
+	  return false;// SigmaIetaIeta Cut
+	}
+      if ( _iso && !photonPassIsoRun1( i, _debug ) ) return false;//Apply Isolation if flag (_iso) is true
       //if ( _iso) std::cout << "yes, passed ISO" << std::endl;
     }
   else
     {
       //EE
-      //std::cout << "=====EE====" << std::endl;
-      //std::cout << "pass Ele Veto?" << std::endl;
-      if ( EE_EleVeto && !passEleVetoRun1( i ) ) return false;
-      //std::cout<< "yes!, pass HoverE?" << std::endl;
-      if ( pho_HoverE[i] > EE_HoverECut ) return false;// HoverE Cut 
-      //std::cout<< "yes!, pass ietaieta?" << std::endl;
-      if ( phoSigmaIetaIeta[i] > EE_SigmaIetaIetaCut ) return false;// SigmaIetaIeta Cut
-      //if ( phoFull5x5SigmaIetaIeta[i] > EE_SigmaIetaIetaCut ) return false;
-      //std::cout<< "yes!" << std::endl;
-      if ( _iso && !photonPassIsoRun1( i ) ) return false;//Apply Isolation if flag (_iso) is true
-      //if ( _iso) std::cout<< "yes, passed ISO" << std::endl;
+      if ( EE_EleVeto && !passEleVetoRun1( i ) )
+	{
+	  if ( _debug ) std::cout << "EE, ID: failed CSEV" << std::endl;
+	  return false;
+	}
+      if ( pho_HoverE[i] > EE_HoverECut )
+	{
+	  if ( _debug ) std::cout << "EE, ID: failed HoveE: " << pho_HoverE[i] << std::endl;
+	  return false;// HoverE Cut 
+	}
+      if ( phoSigmaIetaIeta[i] > EE_SigmaIetaIetaCut )
+	{
+	  if ( _debug ) std::cout << "EE, ID: failed SigmaIetaIeta: " << phoSigmaIetaIeta[i] <<  std::endl;
+	  return false;// SigmaIetaIeta Cut
+	}
+      if ( _iso && !photonPassIsoRun1( i, _debug ) ) return false;//Apply Isolation if flag (_iso) is true
     }
 
   return true;
