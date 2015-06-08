81a7a18c684a208a8eee54a10819d3ac1feafcfa
Javier Duarte <jduarte@caltech.edu>
2015-06-08 16:16:10 -0700

    if datasets already made, don't remake

--- python/RunCombine.py
+++ python/RunCombine.py
@@ -3,8 +3,7 @@ import os
 import ROOT as rt
 from array import *
 import sys
-
-
+import glob
 
 if __name__ == '__main__':
 
@@ -30,7 +29,6 @@ if __name__ == '__main__':
     parser.add_option('--fit',dest="fit",default=False,action='store_true',
                   help="Turn on pre-fit")
 
-
     (options,args) = parser.parse_args()
 
     boxes = options.box.split('_')
@@ -53,12 +51,14 @@ if __name__ == '__main__':
         fit = '--fit'
         
     for lumi in lumiArray:
-        for box in boxes:
-            os.system('python python/DustinTuple2RooDataSet.py -c %s -b %s -d Datasets/ -w -q Backgrounds/*.root'%(cfg,box))
-            os.system('python python/DustinTuple2RooDataSet.py -c %s -b %s -d Datasets/ -w Signals/RazorInclusive_SMS-%s_2J_%s_*.root'%(cfg,box,model,massPoint))
-            os.system('python python/RooDataSet2UnweightedDataSet.py -c %s -b %s -d Datasets/ Datasets/RazorInclusive_SMCocktail_weighted_lumi-%.1f_%s_%s.root'%(cfg,box,lumi,btag,box))
+        for box in boxes:             
             signalDsName = 'Datasets/RazorInclusive_SMS-%s_2J_%s_weighted_lumi-%.1f_%s_%s.root'%(model,massPoint,lumi,btag,box)
-            backgroundDsName = 'Datasets/RazorInclusive_SMCocktail_unweighted_lumi-%.1f_%s_%s.root'%(lumi,btag,box)
+            backgroundDsName = 'Datasets/RazorInclusive_SMCocktail_unweighted_lumi-%.1f_%s_%s.root'%(lumi,btag,box)           
+            if not glob.glob(signalDsName):
+                os.system('python python/DustinTuple2RooDataSet.py -c %s -b %s -d Datasets/ -w Signals/RazorInclusive_SMS-%s_2J_%s_*.root'%(cfg,box,model,massPoint))
+            if not glob.glob(backgroundDsName):
+                os.system('python python/DustinTuple2RooDataSet.py -c %s -b %s -d Datasets/ -w -q Backgrounds/*.root'%(cfg,box))
+                os.system('python python/RooDataSet2UnweightedDataSet.py -c %s -b %s -d Datasets/ Datasets/RazorInclusive_SMCocktail_weighted_lumi-%.1f_%s_%s.root'%(cfg,box,lumi,btag,box))
             os.system('python python/WriteDataCard.py -l %f -c %s -b %s -d %s %s %s %s'%(1000*lumi,cfg,box,options.outDir,fit,signalDsName,backgroundDsName))
             
             if signif:
