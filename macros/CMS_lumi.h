9d6e161a9cdf19ed3cdc9d0773f029725551f839
Artur Apresyan <apresyan@gmail.com>
2015-08-17 03:11:32 +0200

    fixes

--- macros/CMS_lumi.h
+++ macros/CMS_lumi.h
@@ -12,7 +12,7 @@ TString cmsText     = "CMS";
 float cmsTextFont   = 61;  // default is helvetic-bold
 
 bool writeExtraText = false;
-TString extraText   = "Preliminary";
+TString extraText   = " Preliminary";
 float extraTextFont = 52;  // default is helvetica-italics
 
 // text sizes and text offsets with respect to the top frame
