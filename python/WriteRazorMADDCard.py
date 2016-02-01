f2098976adef50930d3126e7087e5f5ee2368868
Dustin <dustin.james.anderson@cern.ch>
2016-02-01 19:36:41 +0100

    Enable combining boxes

--- python/WriteRazorMADDCard.py
+++ python/WriteRazorMADDCard.py
@@ -10,7 +10,6 @@ from DustinTuples2DataCard import uncorrelate, writeDataCard_th1
 import macro.macro as macro
 
 LUMI = 2185 #in /pb
-MCLUMI = 1 
 
 SAMPLES_HADRONIC = ["Other", "QCD", "DYJets", "ZInv", "SingleTop", "WJets", "TTJets2L", "TTJets1L"]
 SAMPLES_LEPTONIC = ["Other", "DYJets", "ZInv", "SingleTop", "WJets", "TTJets1L", "TTJets2L"]
@@ -48,53 +47,72 @@ if __name__ == "__main__":
         print "Please choose an analysis box with --box"
         sys.exit()
     box = args.box
+    boxList = box.split('_') #interpret box1_box2_... as a list of boxes to combine
+
     #make output directory
     os.system('mkdir -p '+dirName)
-    #get configuration for this box
-    samples = SAMPLES[box]
-    unrollBins = [(xbinsSignal[box][str(btags)+'B'], colsSignal[box][str(btags)+'B']) for btags in range(4)]
 
-    #make combined unrolled histograms for background
-    backgroundHists = unrollAndStitch(box, samples=samples, directory=dirName, unrollBins=unrollBins, noSys=args.noSys, addStatUnc=(not args.noStat), debugLevel=debugLevel)
+    for curBox in boxList:
+        #get configuration for this box
+        samples = SAMPLES[curBox]
+        unrollBins = [(xbinsSignal[curBox][str(btags)+'B'], colsSignal[curBox][str(btags)+'B']) for btags in range(4)]
 
-    #treat appropriate uncertainties as uncorrelated bin to bin
-    toUncorrelate = ['ttcrosscheck','zllcrosscheck','btagcrosscheckmr','btagcrosscheckrsq','btaginvcrosscheck','vetolepetacrosscheck','vetotauetacrosscheck','vetolepptcrosscheck','vetotauptcrosscheck']
-    toUncorrelate += ['stat'+box+sample for sample in samples]
-    for sys in toUncorrelate:
-        if 'stat' in sys:
-            if args.noStat: continue
-            suppressLevel = 0.1
+        #make combined unrolled histograms for background
+        backgroundHists = unrollAndStitch(curBox, samples=samples, directory=dirName, unrollBins=unrollBins, noSys=args.noSys, addStatUnc=(not args.noStat), debugLevel=debugLevel)
+
+        #treat appropriate uncertainties as uncorrelated bin to bin
+        toUncorrelate = ['ttcrosscheck','zllcrosscheck','btagcrosscheckmr','btagcrosscheckrsq','btaginvcrosscheck','vetolepetacrosscheck','vetotauetacrosscheck','vetolepptcrosscheck','vetotauptcrosscheck']
+        toUncorrelate += ['stat'+curBox+sample for sample in samples]
+        for sys in toUncorrelate:
+            if 'stat' in sys:
+                if args.noStat: continue
+                suppressLevel = 0.1
+            else:
+                if args.noSys: continue
+                suppressLevel = 0.0
+            uncorrelate(backgroundHists, sys, suppressLevel=suppressLevel)
+
+        #call SMS template maker
+        if 'T1' in args.model:
+            modelName = 'SMS-'+args.model+'_'+str(args.mGluino)+'_'+str(args.mLSP)
         else:
-            if args.noSys: continue
-            suppressLevel = 0.0
-        uncorrelate(backgroundHists, sys, suppressLevel=suppressLevel)
+            modelName = 'SMS-'+args.model+'_'+str(args.mStop)+'_'+str(args.mLSP)
+        signalFilename=SIGNAL_DIR+'/'+modelName+'.root'
+        exec_me('python python/SMSTemplates.py --merge-bins -c %s -d %s --lumi %d --box %s %s %s'%(config, dirName, LUMI, curBox, ((args.noSys)*('--no-signal-sys')), signalFilename), False) 
+        #load SMS template histograms
+        signalHistFilename = '%s/%s_lumi-%.3f_0-3btag_%s.root'%(dirName,modelName,LUMI*1.0/1000,curBox)
+        signalHists = macro.importHists(signalHistFilename)
+        #update with correct names
+        for x,h in signalHists.items():
+            h.SetName(h.GetName().replace(curBox+'_'+args.model,modelName))
+            signalHists[h.GetName()] = signalHists.pop(x)
+        #combine signal and background dictionaries
+        hists = backgroundHists.copy()
+        hists.update(signalHists)
 
-    #call SMS template maker
-    if 'T1' in args.model:
-        modelName = 'SMS-'+args.model+'_'+str(args.mGluino)+'_'+str(args.mLSP)
-    else:
-        modelName = 'SMS-'+args.model+'_'+str(args.mStop)+'_'+str(args.mLSP)
-    signalFilename=SIGNAL_DIR+'/'+modelName+'.root'
-    exec_me('python python/SMSTemplates.py --merge-bins -c %s -d %s --lumi %d --box %s %s %s'%(config, dirName, LUMI, box, ((args.noSys)*('--no-signal-sys')), signalFilename), False) 
-    #load SMS template histograms
-    signalHistFilename = '%s/%s_lumi-%.3f_0-3btag_%s.root'%(dirName,modelName,LUMI*1.0/1000,box)
-    signalHists = macro.importHists(signalHistFilename)
-    #update with correct names
-    for x,h in signalHists.items():
-        h.SetName(h.GetName().replace(box+'_'+args.model,modelName))
-        signalHists[h.GetName()] = signalHists.pop(x)
-    #combine signal and background dictionaries
-    hists = backgroundHists.copy()
-    hists.update(signalHists)
+        #write histograms to ROOT file
+        outFileName = dirName+'/RazorInclusiveMADD_lumi-%.1f_%s.root'%(LUMI*1.0/1000.,curBox)
+        outFile = rt.TFile(outFileName, 'recreate')
+        for x,h in hists.iteritems():
+            h.Write()
+        outFile.Close()
+        #write combine card
+        cardName = outFileName.replace('.root','.txt')
+        writeDataCard_th1(curBox,modelName,cardName,hists,samples)
 
-    #write histograms to ROOT file
-    outFileName = dirName+'/RazorInclusiveMADD_lumi-%.1f_%s.root'%(LUMI*1.0/1000.,box)
-    outFile = rt.TFile(outFileName, 'recreate')
-    for x,h in hists.iteritems():
-        h.Write()
-    outFile.Close()
-    #write combine card
-    cardName = outFileName.replace('.root','.txt')
-    writeDataCard_th1(box,modelName,cardName,hists,samples)
     #run combine
-    exec_me('combine -M Asymptotic '+cardName, False) 
+    if len(boxList) == 1:
+        #get card name
+        cardName = dirName+'/RazorInclusiveMADD_lumi-%.1f_%s.txt'%(LUMI*1.0/1000.,boxList[0])
+        exec_me('combine -M Asymptotic '+cardName, False)
+    elif len(boxList) > 1:
+        #get card names
+        cardNames = []
+        for curBox in boxList:
+            cardName = 'RazorInclusiveMADD_lumi-%.1f_%s.txt'%(LUMI*1.0/1000.,curBox)
+            cardNames.append(cardName)
+        #combine cards
+        combinedCardName = 'RazorInclusiveMADD_'+('_'.join(boxList))+'.txt'
+        exec_me('cd '+dirName+'; combineCards.py '+(' '.join(cardNames))+' > '+combinedCardName+'; cd ..', False)
+        #call combine
+        exec_me('combine -M Asymptotic '+dirName+'/'+combinedCardName, False)
