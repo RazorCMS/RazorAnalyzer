4d11fbb1506e25200e61d160923a8c759d871db7
Javier <jduarte@caltech.edu>
2016-04-18 07:07:25 +0200

    new tag

--- python/RunCombineJobs.py
+++ python/RunCombineJobs.py
@@ -62,7 +62,7 @@ def writeBashScript(box,btag,model,mg,mchi,lumi,config,submitDir,isData,fit,pena
     script += 'pwd\n'
     script += 'git clone git@github.com:RazorCMS/RazorAnalyzer\n'
     script += 'cd RazorAnalyzer\n'
-    script += 'git checkout -b Limits Limits20160415\n'
+    script += 'git checkout -b Limits Limits20160417\n'
     script += 'source setup.sh\n'
     script += 'make\n'
     script += 'mkdir -p Datasets\n'
