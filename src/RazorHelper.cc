70922aba0f1711969e933369333004a298324fc7
SiXie <Si.Xie@cern.ch>
2016-06-29 20:40:33 +0200

    add all single lepton triggers to our single lepton trigger list

--- src/RazorHelper.cc
+++ src/RazorHelper.cc
@@ -525,8 +525,8 @@ void RazorHelper::loadTrigger_Razor2016() {
     hadronicTriggerNums = std::vector<int>(13);
     if( isData ) {
         dileptonTriggerNums = { 44,45,57,59,64,65,66,67,68 };
-        singleLeptonTriggerNums = std::vector<int>(23);
-        singleLeptonTriggerNums = { 4,6,12,13,18,19,20,24,27,28,29,30,31,32,33,34,35,36,37,38,39,42,43 };
+        singleLeptonTriggerNums = std::vector<int>(41);
+        singleLeptonTriggerNums = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43 };
         hadronicTriggerNums = { 164,165,166,167,168,169,170,171,172,173,174,175,176 };
     }
     else {
