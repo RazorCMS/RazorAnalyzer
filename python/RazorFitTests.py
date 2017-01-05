165f8a88f5e231ed575e12b149b9875f9ce2e69e
Dustin <dustin.james.anderson@cern.ch>
2017-01-05 03:13:48 +0100

    fit class now runs toys

--- python/RazorFitTests.py
+++ python/RazorFitTests.py
@@ -14,14 +14,21 @@ if __name__ == '__main__':
             action='store_true')
     parser.add_argument('--load', help='load dataset from file',
             action='store_true')
-    parser.add_argument('--no-fit', dest='noFit', action='store_true',
+    parser.add_argument('--load-fit', dest='loadFit', action='store_true',
             help='Create dataset but do not fit')
     parser.add_argument('--no-plot', dest='noPlot', action='store_true',
             help='Do not make plots')
+    parser.add_argument('--unblind', action='store_true',
+            help='Do not blind signal sensitive region')
+    parser.add_argument('--run-toys', action='store_true',
+            help='Run toy generation', dest='runToys')
+    parser.add_argument('--plot-toys', action='store_true',
+            help='Plot uncertainties from toys', dest='plotToys')
     args = parser.parse_args()
 
     weights = {}
     fitter = FitInstance(args.box, tag=args.tag, isData=not args.mc, 
             weights=weights, full=args.full)
-    fitter.doFitSequence(load=args.load, doFit=(not args.noFit),
-            plot=(not args.noPlot))
+    fitter.doFitSequence(load=args.load, doFit=(not args.loadFit),
+            plot=(not args.noPlot), unblind=args.unblind, runToys=args.runToys,
+            plotToys=args.plotToys)
--- python/macro/razorFits.py
+++ python/macro/razorFits.py
@@ -4,14 +4,15 @@ import ROOT as rt
 from array import array
 
 # Local imports
-from razorAnalysis import Analysis
 import macro
+from razorAnalysis import Analysis
 from framework import Config
 from DustinTuple2RooDataSet import convertTree2Dataset, initializeWorkspace
+from BinnedFit import binnedFit
 from WriteDataCard import convertDataset2TH1
 from WriteDataCard import initializeWorkspace as initializeFitWorkspace
-from PlotFit import setStyle, convertSideband, get3DHistoFrom1D, print1DProj, get1DHistoFrom2D, print2DResiduals
-from BinnedFit import binnedFit
+from RunToys import runToys
+from PlotFit import setStyle, convertSideband, get3DHistoFrom1D, print1DProj, print1DProjNs, get1DHistoFrom2D, print2DResiduals, getErrors1D, getErrors2D, getErrors3D, getNsigma2D
 
 def unweightHist(hist):
     """Throws a Poisson toy from each bin of the histogram.
@@ -71,6 +72,9 @@ class FitInstance(object):
           isData: bool
           workspace: RooWorkspace to hold dataset, fit result, fit function, etc
           filename: name of ROOT file used to hold workspace
+          dirname: name of directory for output files
+          toysFile: path to file containing stat+sys toys
+          sysFile: path to file containing sys-only toys
           config: Config object defining fit function parameters and binning
           fitRegion: either 'Full' or 'LowMR,LowRsq'
           sideband: string containing sideband bin information
@@ -89,6 +93,11 @@ class FitInstance(object):
             self.fitRegion = 'LowMR,LowRsq'
         self.analysis = Analysis(box, tag=tag)
         self.filename = self.getFilename()
+        self.dirname = os.path.dirname(self.filename)
+        self.toysFile = self.getToysFilename(noStat=False)
+        self.sysFile = self.getToysFilename(noStat=True)
+        if not os.path.isdir(self.dirname):
+            os.makedirs(self.dirname)
         self.config = Config.Config(configFile)
         self.x = array('d', self.config.getBinning(box)[0]) # MR binning
         self.y = array('d', self.config.getBinning(box)[1]) # Rsq binning
@@ -118,7 +127,24 @@ class FitInstance(object):
             if self.weights is not None:
                 for proc,weight in self.weights.iteritems():
                     strs.append(("%s_%.2f"%(proc,weight)).replace(".","p"))
-        return "_".join(strs)+".root"
+        filename = "_".join(strs)+".root"
+        return self.getDirname(filename)+"/"+filename
+
+    def getDirname(self, filename):
+        """Builds the name of the output directory"""
+        dirname = "Plots/%s/Fits_%s"%(self.analysis.tag, 
+                filename.replace(".root","").replace(
+                "RazorFitInstance_%s_"%self.analysis.tag,""))
+        return dirname
+
+    def getToysFilename(self, noStat=False):
+        """Gets the appropriate toys filename"""
+        if noStat:
+            unc = "noStat"
+        else:
+            unc = "varyN"
+        fname = "toys_Bayes_%s_%s.root"%(unc, self.analysis.region)
+        return self.dirname+"/"+fname
 
     def getTrees(self):
         """Opens the ntuple files and loads the tree from each"""
@@ -201,7 +227,7 @@ class FitInstance(object):
         result.Print('v')
         self.addToWorkspace(result, tobject=True)
 
-    def get3DFitHistos(self):
+    def get3DFitHistos(self, sideband):
         """Returns a pair of TH3s: (data, fit prediction)"""
         extRazorPdf = self.workspace.pdf('extRazorPdf')
         th1x = self.workspace.var('th1x')
@@ -209,7 +235,7 @@ class FitInstance(object):
         asimov = extRazorPdf.generateBinned(rt.RooArgSet(th1x),
                 rt.RooFit.Name('central'),rt.RooFit.Asimov())
         opt = [rt.RooFit.CutRange(myRange) 
-                for myRange in self.sideband.split(',')]
+                for myRange in sideband.split(',')]
         asimov_reduce = asimov.reduce(opt[0])
         dataHist_reduce = dataHist.reduce(opt[0])
         for iOpt in range(1,len(opt)):
@@ -222,6 +248,8 @@ class FitInstance(object):
         return h_data_nBtagRsqMR, h_nBtagRsqMR
 
     def getSidebandMax(self):
+        """Returns a list containing the upper edge values of the
+            MR and Rsq sidebands"""
         sideband = None
         if self.fitRegion=="LowMR,LowRsq":
             mrSide = self.workspace.var('MR').getMax('LowMR')
@@ -229,12 +257,83 @@ class FitInstance(object):
             sideband = [mrSide, rsqSide]
         return sideband
 
-    def plot(self, filename=None):
-        """Plots the fit results (before toys)"""
+    def runToys(self, sysPlusStat=True, numToys=1000):
+        """Call the toy-generation machinery"""
+        # This is a dummy class to hold options for runToys()
+        class Options(object):
+            pass
+        opts = Options()
+        opts.r = -1
+        opts.box = self.analysis.region
+        opts.fitRegion = self.fitRegion
+        opts.varyN = sysPlusStat
+        opts.noStat = (not sysPlusStat)
+        opts.noSys = False
+        opts.freq = False
+        opts.oneSigma = False
+        opts.outDir = self.dirname
+        opts.nToys = numToys
+        toysFile = runToys(self.workspace, opts, self.config, seed=-1)
+        print "Uncertainty information stored in %s"%toysFile
+        return toysFile
+
+    def getToyTree(self, inputFile=None):
+        """Get tree from file"""
+        toyTree = None
+        if inputFile is not None:
+            toyFiles = inputFile.split(',')
+            toyTree = rt.TChain("myTree")
+            for toyFile in toyFiles:
+                toyTree.Add(toyFile)
+            toyTree.SetName("toyTree")
+        return toyTree
+
+    def blindSideband(self, hist, plotRegion):
+        w = self.workspace
+        if plotRegion!='Full':            
+            for i in range(1,hist.GetNbinsX()+1):
+                for j in range(1,hist.GetNbinsY()+1):
+                    w.var('MR').setVal(hist.GetXaxis().GetBinCenter(i))
+                    w.var('Rsq').setVal(hist.GetYaxis().GetBinCenter(j))
+                    w.var('nBtag').setVal(self.z[k-1])
+                    inSideband = 0
+                    for fitname in plotRegion.split(','):
+                        inSideband += ( w.var('MR').inRange(fitname) 
+                                * w.var('Rsq').inRange(fitname) 
+                                * w.var('nBtag').inRange(fitname) )
+                    if not inSideband:
+                        hist.SetBinContent(i,j,0)
+
+    def getPlottingOptions(self):
+        """Wraps appropriate plotting options in an object and returns it"""
+        class Options(object):
+            pass
+        options = Options()
+        options.fitRegion = self.fitRegion
+        options.noStat = True
+        options.printErrors = True
+        options.outDir = self.dirname
+        return options
+
+    def plot(self, filename=None, unblind=False, 
+            toysFile=None, sysFile=None):
+        """Plots the fit results"""
+        options = self.getPlottingOptions()
+        if unblind:
+            plotRegion = 'Full'
+        else:
+            plotRegion = self.fitRegion
         if filename is None:
             filename = self.filename.replace('.root','_Plots.root')
         f = rt.TFile(filename, 'UPDATE')
-        dirName = "BeforeToys"
+        toyTree = self.getToyTree(toysFile)
+        sysTree = self.getToyTree(sysFile)
+        if toyTree is not None and sysTree is not None:
+            dirName = "WithToys"
+            computeErrors = True
+        else:
+            dirName = "BeforeToys"
+            computeErrors = False
         tdirectory = f.GetDirectory(dirName)
         if tdirectory==None:
             f.mkdir(dirName)
@@ -244,14 +343,29 @@ class FitInstance(object):
         rt.TH1D.SetDefaultSumw2()
         rt.TH2D.SetDefaultSumw2()
         rt.TH3D.SetDefaultSumw2()
-        if self.sideband is None:
-            self.sideband = convertSideband(self.fitRegion, self.workspace, 
-                    self.x, self.y, self.z)
+        plotband = convertSideband(plotRegion, self.workspace,
+                self.x, self.y, self.z)
 
-        h_data_nBtagRsqMR, h_nBtagRsqMR = self.get3DFitHistos()
-        data_projections = make3DHistProjections(h_data_nBtagRsqMR)
-        fit_projections = make3DHistProjections(h_nBtagRsqMR)
-        for h in data_projections+fit_projections:
+        # Get the 1D and 2D histograms for each b-tag bin
+        h_data_nBtagRsqMR, h_nBtagRsqMR = self.get3DFitHistos(plotband)
+        h_data_MR,h_data_Rsq,h_data_RsqMR = make3DHistProjections(
+                h_data_nBtagRsqMR)
+        h_MR,h_Rsq,h_RsqMR = make3DHistProjections(h_nBtagRsqMR)
+        if computeErrors:
+            h_MR = getErrors1D(h_MR,h_data_MR,sysTree,options,"x",0,
+                    len(self.x)-1,0,len(self.y)-1,0,len(self.z)-1,
+                    self.x,self.y,self.z)
+            h_Rsq = getErrors1D(h_Rsq,h_data_Rsq,sysTree,options,"y",0,
+                    len(self.x)-1,0,len(self.y)-1,0,len(self.z)-1,
+                    self.x,self.y,self.z)
+            h_RsqMR = getErrors2D(h_RsqMR,h_data_RsqMR,sysTree,options,"yx",0,
+                    len(self.x)-1,0,len(self.y)-1,0,len(self.z)-1,
+                    self.x,self.y,self.z)
+            h_nBtagRsqMR = getErrors3D(h_nBtagRsqMR,h_data_nBtagRsqMR,sysTree,
+                    options,"zyx",0,len(self.x)-1,0,len(self.y)-1,0,
+                    len(self.z)-1,self.x,self.y,self.z)
+        for h in [h_data_MR,h_data_Rsq,h_data_RsqMR,h_data_nBtagRsqMR,
+                h_MR,h_Rsq,h_RsqMR,h_nBtagRsqMR]:
             tdirectory.cd()
             h.Write()
         if len(self.z)>1:
@@ -276,6 +390,11 @@ class FitInstance(object):
                 h_data_nBtagRsqMR.GetZaxis().SetRange(k,k)
                 h_data_RsqMR_components.append(h_data_nBtagRsqMR.Project3D(
                     "%ibtag_yx"%self.z[k-1]))
+                if computeErrors:
+                    h_RsqMR_components[-1] = getErrors2D(h_RsqMR_components[-1],
+                            h_data_RsqMR_components[-1],sysTree,options,
+                            "yx",0,len(self.x)-1,0,len(self.y)-1,k,k,
+                            self.x,self.y,self.z)
                 h_th1x_components.append(get1DHistoFrom2D(
                     h_RsqMR_components[-1],self.x,self.y,
                     'h_th1x_%ibtag'%(self.z[k-1])))
@@ -289,33 +408,43 @@ class FitInstance(object):
                 else:            
                     h_labels.append("%i b-tag" % self.z[k-1] )
 
+        # Create the nsigma histograms
         h_RsqMR_statnsigma_components = []
+        h_RsqMR_nsigma_components = []
         if len(self.z)>1:
             for k in range(1,len(self.z)):
                 h_RsqMR_statnsigma_components.append( 
                         getStatNSigmaHist(h_RsqMR_components[k-1], 
                             h_data_RsqMR_components[k-1],
                             "h_RsqMR_statnsigma_%ibtag"%self.z[k-1]))
+                h_RsqMR_nsigma_btag = h_RsqMR.Clone(
+                        "h_RsqMR_nsigma_%ibtag"%self.z[k-1])
+                if computeErrors:
+                    h_RsqMR_nsigma_btag = getNsigma2D(h_RsqMR_nsigma_btag,
+                            h_data_RsqMR_components[k-1],toyTree,options,
+                            "yx",0,len(self.x)-1,0,len(self.y)-1,k,k,
+                            self.x,self.y,self.z)
+                    self.blindSideband(h_RsqMR_nsigma_btag, plotRegion)
+                    h_RsqMR_nsigma_components.append(h_RsqMR_nsigma_btag)
 
+        # Print everything out to pdf files
         btagLabel = getBtagLabel(self.z)
         lumiLabel = "%.0f fb^{-1} (13 TeV)" % (self.analysis.lumi/1000.)
         boxLabel = "razor %s %s %s Fit" % (self.analysis.region,
                 btagLabel,self.fitRegion.replace('LowMR,LowRsq','Sideband'))
         plotLabel = ""
+        eventsLabel = "Events"
         sidebandFit = self.getSidebandMax()
         for h in h_RsqMR_components:
             tdirectory.cd()
             h.Write()
-        plotDir = "Plots/%s/Fits"%(self.analysis.tag)
-        if not os.path.isdir(plotDir):
-            os.makedirs(plotDir)
-        print1DProj(c,tdirectory,fit_projections[0],data_projections[0],
-                plotDir+"/h_MR_%s.pdf"%self.analysis.region,"M_{R} [GeV]",
-                "Events",lumiLabel,boxLabel,plotLabel,self.isData,False,None,
+        print1DProj(c,tdirectory,h_MR,h_data_MR,
+                self.dirname+"/h_MR_%s.pdf"%self.analysis.region,"M_{R} [GeV]",
+                eventsLabel,lumiLabel,boxLabel,plotLabel,self.isData,False,None,
                 None,h_MR_components,h_colors,h_labels)
-        print1DProj(c,tdirectory,fit_projections[1],data_projections[1],
-                plotDir+"/h_Rsq_%s.pdf"%self.analysis.region,"R^{2}",
-                "Events",lumiLabel,boxLabel,plotLabel,self.isData,False,None,
+        print1DProj(c,tdirectory,h_Rsq,h_data_Rsq,
+                self.dirname+"/h_Rsq_%s.pdf"%self.analysis.region,"R^{2}",
+                eventsLabel,lumiLabel,boxLabel,plotLabel,self.isData,False,None,
                 None,h_Rsq_components,h_colors,h_labels)
         if len(self.z)>2:
             for k in range(0,len(self.z)-1):
@@ -323,19 +452,35 @@ class FitInstance(object):
                     h_labels[k],self.fitRegion.replace('LowMR,LowRsq','Sideband'))
                 print1DProj(c,tdirectory,h_th1x_components[k],
                     h_data_th1x_components[k], 
-                    plotDir+"/h_th1x_%ibtag_%s.pdf"%(self.z[k],
+                    self.dirname+"/h_th1x_%ibtag_%s.pdf"%(self.z[k],
                         self.analysis.region),"Bin Number",
-                        "Events",lumiLabel,newBoxLabel,plotLabel,self.isData,
-                        False, None)
+                        eventsLabel,lumiLabel,newBoxLabel,plotLabel,self.isData,
+                        False, options)
                 print2DResiduals(c,tdirectory,h_RsqMR_statnsigma_components[k],
-                    plotDir+"/h_RsqMR_statnsigma_log_%ibtag_%s.pdf"%(self.z[k],
+                    self.dirname+"/h_RsqMR_statnsigma_log_%ibtag_%s.pdf"%(self.z[k],
                     self.analysis.region), "M_{R} [GeV]", "R^{2}", 
                     "Stat. n#sigma (Data - Fit)/sqrt(Fit)",
                     lumiLabel,newBoxLabel,plotLabel,self.x,self.y,self.isData,
-                    sidebandFit,False,None)
+                    sidebandFit,False,options)
+                if computeErrors:
+                    # This function does not seem to work right now?
+                    #print1DProjNs(c,tdirectory,h_th1x_components[k],
+                    #        h_data_th1x_components[k],
+                    #        h_RsqMR_nsigma_components[k],
+                    #        self.dirname+"/h_th1x_ns_%ibtag_%s.pdf"%(
+                    #            self.z[k],self.analysis.region),
+                    #        "Bin Number",eventsLabel,lumiLabel,newBoxLabel,
+                    #        plotLabel,self.isData,False,options) 
+                    print2DResiduals(c,tdirectory,h_RsqMR_nsigma_components[k],
+                            self.dirname+"/h_RsqMR_nsigma_log_%ibtag_%s.pdf"%(
+                            self.z[k],self.analysis.region),"M_{R} [GeV]", 
+                            "R^{2}","Stat.+Sys. n#sigma",lumiLabel,newBoxLabel,
+                            plotLabel, self.x,self.y,self.isData,sidebandFit,
+                            False,options)
         f.Close()
 
-    def doFitSequence(self, load=False, doFit=True, plot=True):
+    def doFitSequence(self, load=False, doFit=True, plot=True, unblind=False,
+            runToys=False, plotToys=False):
         """Performs all steps needed to build and fit the dataset"""
         if load:
             self.loadWorkspace()
@@ -344,6 +489,15 @@ class FitInstance(object):
             self.initBinnedDataset()
         if doFit:
             self.fit()
+        if runToys:
+            self.runToys(sysPlusStat=True)
+            self.runToys(sysPlusStat=False)
         if plot:
-            self.plot()
+            toysFile = None
+            sysFile = None
+            if runToys or plotToys:
+                toysFile = self.toysFile
+                sysFile = self.sysFile
+            self.plot(unblind=unblind, toysFile=toysFile,
+                    sysFile=sysFile)
         self.writeWorkspace()
