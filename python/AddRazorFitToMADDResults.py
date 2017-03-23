271c47f87a56a778714093f113789a11262259d3
Dustin <dustin.james.anderson@cern.ch>
2017-03-23 02:48:39 +0100

    Enable combining fits from individual b-tag bins
    The new script CombineBTagToys.py combines razor toy files from
    different b-tag bins into one file suitable for plotting.
    I also decreased the verbosity of the code for loading and saving
    histograms.

--- python/AddRazorFitToMADDResults.py
+++ python/AddRazorFitToMADDResults.py
@@ -12,8 +12,15 @@ if __name__ == '__main__':
     parser.add_argument("--tag", default="Razor2016_MoriondRereco",
             help="Analysis tag, e.g. Razor2015")
     parser.add_argument('--box', help="choose a box", required=True)
+    parser.add_argument('--force', help="override existing fit histograms",
+            action='store_true')
+    # Specifying a different input directory of fit file
     parser.add_argument('--no-sys', help='no systematics', 
             action='store_true', dest='noSys')
+    parser.add_argument('--in-dir', help='input directory',
+            dest='inDir')
+    parser.add_argument('--fit-file', help='file with fit toys',
+            dest='fitFile')
     args = parser.parse_args()
     
     c = rt.TCanvas("c","c",400,300) # dummy canvas
@@ -21,17 +28,26 @@ if __name__ == '__main__':
     btagsMax = 3
     if args.box in ['DiJet','LeptonJet']:
         btagsMax = 2
+
+    # Input directory
+    baseDir = "Plots/%s"%args.tag
+    if args.inDir is not None:
+        baseDir = args.inDir
+
+    # Fit file
+    fitFile = razorFitFiles[args.tag][args.box]
+    if args.fitFile is not None:
+        fitFile = args.fitFile
+
     for btags in range(btagsMax+1):
-        inDir = "Plots/%s/%s%dB"%(args.tag, args.box, btags)
+        inDir = "%s/%s%dB"%(baseDir, args.box, btags)
         if args.noSys:
             inDir += 'NoSys'
         inFile = "razorHistograms%s%dB.root"%(args.box, btags)
         if args.noSys:
             inFile = inFile.replace(".root","NoSys.root")
         hists = macro.importHists(inDir+'/'+inFile)
-        if 'Fit' in hists:
-            sys.exit("Fit histograms already exist in file!")
-        import2DRazorFitHistograms(hists, bins, 
-                razorFitFiles[args.tag][args.box], c, 
-                "Data", btags, btagsMax, noStat=True)
+
+        import2DRazorFitHistograms(hists, bins, fitFile, c, 
+                "Data", btags, btagsMax, noStat=True, force=args.force)
         macro.exportHists(hists, outFileName=inFile, outDir=inDir)
