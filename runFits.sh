0f13bd9fe4e55fe2b82d765377134838425a22d0
Jay <jlawhorn@mit.edu>
2017-04-11 14:35:06 +0200

    commit updated fit script

--- runFits.sh
+++ runFits.sh
@@ -1,7 +1,6 @@
 
 for cat in DiJet_0b #DiJet_1b DiJet_2b #MultiJet_0b MultiJet_1b MultiJet_2b LeptonJet_0b LeptonJet_1b LeptonJet_2b LeptonMultiJet_0b LeptonMultiJet_1b LeptonMultiJet_2b #DiJet MultiJet LeptonJet LeptonMultiJet
 do
-#echo $cat
-python python/RazorFitMacro.py --config config/run2_2017_03_13_SeparateBtagFits_forToys.config $cat #> ${cat}.txt
-#python python/RazorFitMacro.py --config config/run2_2017_03_13_SeparateBtagFits_forToys.config --load-fit --run-toys $cat #> ${cat}.txt
+python python/RazorFitMacro.py --config config/run2_2017_03_13_SeparateBtagFits.config $cat #> ${cat}.txt
+python python/RazorFitMacro.py --config config/run2_2017_03_13_SeparateBtagFits_forToys.config --input-fit-file Plots/Razor2016_MoriondRereco/Fits_${cat}/RazorFitInstance_Razor2016_MoriondRereco_${cat}.root --run-toys --no-fit $cat 
 done
