4606305d658712ae6c3fc45f0f3a53ac3d1e79e0
SiXie <Si.Xie@cern.ch>
2017-08-08 17:19:13 +0200

    update jamie's code on dark photons

--- analyzers/DarkPhotonAnalyzer.cc
+++ analyzers/DarkPhotonAnalyzer.cc
@@ -68,10 +68,19 @@ void DarkPhotonAnalyzer::Analyze(bool isData, int Option, string outputFilename,
   float MT;
   float j1_Eta;
   float j2_Eta;
+  float l1_PT;
+  float l1_Eta;
+  float l1_Phi;
+  float l2_PT;
+  float l2_Eta;
+  float l2_Phi;
+  int Iso_lepton;
   // working variables:
   float Copy_Jet_PT[900];
-
-  TTree *outputTree = new TTree("HggRazor", "Info on selected razor inclusive events");
+  float Copy_Electron_PT[700];
+  float Copy_Muon_PT[700];
+  
+  TTree *outputTree = new TTree("VBFTree", "Info on selected razor inclusive events");
 
   outputTree->Branch("weight", &weight, "weight/F");
   outputTree->Branch("pileupWeight", &pileupWeight, "pileupWeight/F");
@@ -92,6 +101,13 @@ void DarkPhotonAnalyzer::Analyze(bool isData, int Option, string outputFilename,
   outputTree->Branch("MT", &MT, "MT/F");
   outputTree->Branch("j1_Eta", &j1_Eta, "j1_Eta/F");
   outputTree->Branch("j2_Eta", &j2_Eta, "j2_Eta/F");
+  outputTree->Branch("l1_PT", &l1_PT, "l1_PT/F");
+  outputTree->Branch("l1_Eta", &l1_Eta, "l1_Eta/F");
+  outputTree->Branch("l1_Phi", &l1_Phi, "l1_Phi/F");
+  outputTree->Branch("l2_PT", &l2_PT, "l2_PT/F");
+  outputTree->Branch("l2_Eta", &l2_Eta, "l2_Eta/F");
+  outputTree->Branch("l2_Phi", &l2_Phi, "l2_Phi/F");
+  outputTree->Branch("Iso_lepton", &Iso_lepton, "Iso_lepton/i");	// is there an isolated lepton? (defined according to cuts standards) 1=YES, 0=NO
 
   //begin loop
   if (fChain == 0) return;
@@ -149,7 +165,78 @@ void DarkPhotonAnalyzer::Analyze(bool isData, int Option, string outputFilename,
 	// assign variables
 	j1_Eta = jetEta[j1_index];
 	j2_Eta = jetEta[j2_index];
+	
+	//Fill & select lepton PT, Eta and Phi for the two highest PT leptons here
+	// copy arrays
+	for(int i=0; i<700; i++) {
+		Copy_Electron_PT[i] = elePt[i];	
+	}
+	for(int i=0; i<700; i++) {
+		Copy_Muon_PT[i] = muonPt[i];	
+	}
+	// find indices
+	int e1_index = TMath::LocMax(700,elePt);	// find e1 index
+	Copy_Electron_PT[e1_index] = 0;				// set e1 value in Copy to zero
+	int e2_index = TMath::LocMax(700,Copy_Electron_PT);	// find e2 index
+	//
+	int m1_index = TMath::LocMax(700,muonPt);	// find m1 index
+	Copy_Muon_PT[m1_index] = 0;				// set m1 value in Copy to zero
+	int m2_index = TMath::LocMax(700,Copy_Muon_PT);	// find m2 index
+	//	 	
+	if ( (muonPt[m2_index]<20) && (elePt[e2_index]>20) ) {
+		// select the two electrons
+		l1_PT=elePt[e1_index];
+		l1_Eta=eleEta[e1_index];
+		l1_Phi=elePhi[e1_index];
+		l2_PT=elePt[e2_index];
+		l2_Eta=eleEta[e2_index];
+		l2_Phi=elePhi[e2_index];
+	} else if ( (muonPt[m2_index]>20) && (elePt[e2_index]<20) ) {
+		// select the two muons
+		l1_PT=muonPt[m1_index];
+		l1_Eta=muonEta[m1_index];
+		l1_Phi=muonPhi[m1_index];
+		l2_PT=muonPt[m2_index];
+		l2_Eta=muonEta[m2_index];
+		l2_Phi=muonPhi[m2_index];
+	} else if ( (muonPt[m2_index]<20) && (elePt[e2_index]<20) ) {
+		continue;
+	} else if ( (muonPt[m2_index]>20) && (elePt[e2_index]>20) ) {				// not sure how to deal with both muons and electrons being possible candidate leptons
+		// select the two muons
+		l1_PT=muonPt[m1_index];
+		l1_Eta=muonEta[m1_index];
+		l1_Phi=muonPhi[m1_index];
+		l2_PT=muonPt[m2_index];
+		l2_Eta=muonEta[m2_index];
+		l2_Phi=muonPhi[m2_index];	// default to selecting the muons at the moment
+	}
     
+	// Isolated lepton criterion
+	Iso_lepton = 0;
+	Float_t Phi_l;
+	Float_t Eta_l;
+	Float_t DR;
+	Float_t Pi = TMath::Pi();
+	for (Int_t j=0; j<700; j++) {	
+		Phi_l = TMath::Abs(elePhi[j] - PhotonPhi);
+		if (Phi_l > Pi) {
+			Phi_l = 2*Pi - Phi_l;
+		}
+		Eta_l = TMath::Abs(eleEta[j] - PhotonEta);
+		DR = sqrt(pow(Phi_l,2) + pow((Eta_l - PhotonEta),2));
+		if (DR > 0.3) {									// Isolated electron criterion
+			Iso_lepton = 1;
+		}
+	}
+	for (Int_t j=0; j<700; j++) {	
+		Phi_l = TMath::Abs(muonPhi[j] - PhotonPhi);
+		Eta_l = TMath::Abs(muonEta[j] - PhotonEta);
+		DR = sqrt(pow(Phi_l,2) + pow(Eta_l,2));
+		if (DR > 0.3) {									// Isolated muon criterion
+			Iso_lepton = 1;
+		}
+	}
+	
     //fill normalization histogram    
     NEvents->SetBinContent( 1, NEvents->GetBinContent(1) + genWeight);
     weight = genWeight;
