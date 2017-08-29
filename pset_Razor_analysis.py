454c920e38a2982ddb14c1c513654e417d524946
Dustin <dustin.james.anderson@cern.ch>
2017-08-29 23:06:48 +0200

    remove old unused files
    -fit results from 2015 razor inclusive analysis
    -config files for unused crab and caltech tier3 workflows
    -run scripts for old version of inclusive razor analysis

--- pset_Razor_analysis.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-process = cms.Process('NoSplit')
-
-process.source = cms.Source("EmptySource")
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
-process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
--- runRazorCrab.sh
+++
@@ -1,8 +0,0 @@
-# make the tmplist of files to run over
-sed $1'q;d' listfile.txt | sed 's/eoscms\/\/eos\/cms/xrootd-cms.infn.it\//' > tmplist.txt
-
-# run RazorRun
-./RazorRun tmplist.txt Analyses Option ntupleName.root 
-
-# run cmsRun to produce a valid FrameworkJobReport.xml 
-cmsRun -j FrameworkJobReport.xml -p PSet.py
