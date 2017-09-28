fa3cf1d39ebd948769923de98aa239789dff376a
Dustin <dustin.james.anderson@cern.ch>
2017-09-29 00:59:37 +0200

    allow combining razor inclusive and boost limits
    -Add script to combine the inclusive and boost cards and run Combine
    -Modify GetCombineMADD script so it can retrieve combined limits
    -Enable inclusive+boost combination in limit util tool

--- python/limits/LimitUtils.py
+++ python/limits/LimitUtils.py
@@ -20,14 +20,14 @@ def get_limit_dir(model):
     return '/eos/cms/store/group/phys_susy/razor/Run2Analysis/Limits/RazorInclusive2016/{}/{}'.format(
             VERSION, model)
 
-def write_done_file(model, box):
+def write_done_file(model, box, name='MADD'):
     """
     Creates text file containing filenames of completed jobs.
     Returns the name of the file.
     """
     done_jobs = glob.glob(get_limit_dir(model)
-            +'/higgsCombineMADD_{}_*.root'.format(box))
-    done_file_name = 'done_limits_{}_{}.txt'.format(model, box)
+            +'/higgsCombine{}_*.root'.format(name))
+    done_file_name = 'done_limits_{}_{}_{}.txt'.format(name, model, box)
     with open(done_file_name, 'w') as done_file:
         for f in done_jobs:
             done_file.write(os.path.basename(f)+'\n')
@@ -91,6 +91,24 @@ def submit_combine(model, tag, sms, no_sub=True):
         command.append('--no-sub')
     do_command(command, False)
 
+def submit_boost_combine(model, tag, sms, no_sub=True):
+    """
+    Submits jobs to combine limits for one SMS scan.
+    Combines razor inclusive limit with boost limit.
+    """
+    script = 'python/CombineInclusiveBoostLimits.py'
+    out_dir = get_limit_dir(model)
+    queue = '1nd'
+    done_file_name = write_done_file(model, '_'.join(sms.boxes),
+            name='RazorInclusiveBoost')
+    command = ['python', script, '--model', model,
+            '--dir', out_dir, '--queue', queue,
+            '--done-file', done_file_name, '--boxes']
+    command += sms.boxes
+    if no_sub:
+        command.append('--no-sub')
+    do_command(command, False)
+
 def aggregate(model, tag, sms, no_exec=True):
     """
     Makes a directory containing results from all submodels
@@ -130,7 +148,7 @@ def get_config_dir():
     return 'PlotsSMS/config/{}'.format(VERSION)
 
 def get(model, tag, sms, no_smooth=False, do_combined=False,
-        no_exec=True):
+        do_boost_combined=False, no_exec=True):
     """
     Retrieves limit results.  Args are the same as submit()
     except:
@@ -141,16 +159,26 @@ def get(model, tag, sms, no_smooth=False, do_combined=False,
     contour_script = 'python/Get2DContour.py'
     out_dir = get_limit_dir(model)
     boxes = sms.boxes
-    if do_combined:
+    if do_combined or do_boost_combined:
         boxes = ['_'.join(sms.boxes)]
     for box in boxes:
         print "Box {}".format(box)
-        command = ['python', get_script, '--box', box, '--model', model, 
-                '--dir', out_dir]
+        command = ['python', get_script, '--box', box, '--model', model]
+        if do_boost_combined:
+            comb_dir = out_dir+'/RazorInclusiveBoost'
+            command += ['--combine-name', 'RazorInclusiveBoost']
+            command += ['--in-dir', out_dir]
+            command += ['--dir', comb_dir]
+            do_command(['mkdir', '-p', comb_dir], no_exec)
+        else:
+            command += ['--dir', out_dir]
         do_command(command, no_exec)
         
-        command = ['python', contour_script, '--box', box, '--model', model, 
-                '--dir', out_dir]
+        command = ['python', contour_script, '--box', box, '--model', model]
+        if do_boost_combined:
+            command += ['--dir', comb_dir]
+        else:
+            command += ['--dir', out_dir]
         if not sms.isGluino:
             command += ['--xsec-file', 'data/stop13TeV.txt']
         if no_smooth:
@@ -166,12 +194,14 @@ def get_box_str(box):
     return 'razor_1L'
 
 def write_plot_config(model, box, blind=True, preliminary=True, 
-        lumi=35900, no_exec=True):
+        lumi=35900, no_exec=True, do_boost_combined=False):
     """
     Writes a config file for making SMS plot.
     Returns the name of the config.
     """
     in_dir = get_limit_dir(model)
+    if do_boost_combined:
+        in_dir += '/RazorInclusiveBoost'
     name = '{}_{}'.format(model, box)
     results_file = '{}/{}_results.root'.format(
             in_dir, name)
@@ -214,7 +244,7 @@ def write_plot_config(model, box, blind=True, preliminary=True,
     return out_f
 
 def plot(model, tag, sms, blind=True, preliminary=True, no_smooth=False,
-        no_exec=True, do_combined=False):
+        no_exec=True, do_combined=False, do_boost_combined=False):
     """
     Plots limit results.  Args are the same as submit() except for:
     blind: do not draw observed limit
@@ -224,12 +254,12 @@ def plot(model, tag, sms, blind=True, preliminary=True, no_smooth=False,
     plot_dir = get_plot_dir()
     do_command(['mkdir', '-p', plot_dir], no_exec)
     boxes = sms.boxes 
-    if do_combined:
+    if do_combined or do_boost_combined:
         boxes = ['_'.join(sms.boxes)]
     for box in boxes:
         print "Box: {}".format(box)
         config = write_plot_config(model, box, blind, preliminary, 
-                no_exec=no_exec)
+                no_exec=no_exec, do_boost_combined=do_boost_combined)
         name = '{}/{}_{}_'.format(plot_dir, model, box)
         if blind:
             name += 'Blinded_'
@@ -237,6 +267,8 @@ def plot(model, tag, sms, blind=True, preliminary=True, no_smooth=False,
             name += 'Preliminary_'
         if no_smooth:
             name += 'NoSmooth_'
+        if do_boost_combined:
+            name += 'RazorBoostCombined_'
         script = 'PlotsSMS/python/makeSMSplots.py'
         command = ['python', script, config, name]
         do_command(command, no_exec)
@@ -254,6 +286,8 @@ if __name__ == '__main__':
     parser.add_argument('--submit', action='store_true')
     parser.add_argument('--combined', action='store_true',
             help='do combination of boxes')
+    parser.add_argument('--combined-with-boost', action='store_true',
+            help='combine with razor boost analysis')
     parser.add_argument('--get', action='store_true')
     parser.add_argument('--plot', action='store_true')
     parser.add_argument('--aggregate', action='store_true',
@@ -285,14 +319,17 @@ if __name__ == '__main__':
 
     if args.submit:
         print "Submit limit jobs for {}\n".format(args.model)
-        if args.combined:
+        if args.combined_with_boost:
+            submit_boost_combine(args.model, args.tag, sms, no_exec)
+        elif args.combined:
             submit_combine(args.model, args.tag, sms, no_exec)
         else:
             submit(args.model, args.tag, sms, args.bkg_dir, 
                     args.no_boost_cuts, no_exec)
         sys.exit()
 
-    if (args.aggregate or args.finish) and not args.combined:
+    if (args.aggregate or args.finish) and not (args.combined
+            or args.combined_with_boost):
         if sms.submodels is not None:
             print "Combine limit jobs from all datasets for {}".format(
                     args.model)
@@ -303,10 +340,12 @@ if __name__ == '__main__':
     if args.get or args.finish:
         print "Get limit results for {}\n".format(args.model)
         get(args.model, args.tag, sms, no_smooth=args.no_smooth, 
-                do_combined=args.combined, no_exec=no_exec)
+                do_combined=args.combined, no_exec=no_exec,
+                do_boost_combined=args.combined_with_boost)
 
     if args.plot or args.finish:
         print "Plot limit for {}\n".format(args.model)
         plot(args.model, args.tag, sms, blind=args.blind,
                 preliminary=args.preliminary, no_smooth=args.no_smooth,
-                do_combined=args.combined, no_exec=no_exec)
+                do_combined=args.combined, no_exec=no_exec,
+                do_boost_combined=args.combined_with_boost)
